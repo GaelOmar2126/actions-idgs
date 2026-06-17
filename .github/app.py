d7ae75cb420506ce13900151146d84d5d072213e
Gael Omar <PalaciosOmar2126@outlook.es>
2026-06-17 15:37:25 -0600

    actualiza action para python

--- .github/app.py
+++ .github/app.py
@@ -1,4 +1,14 @@
-print ("Hello, World! running in GitHub Actions!")
 
-for i in [1, 2, 3, 4, 5]:
-    print("XD" * i)
+
+import os
+def main():
+    print ("Hello, World! running in GitHub Actions!")
+
+    for i in [1, 2, 3, 4, 5]:
+        print("XD" * i)
+
+    name = os.getenv("USERNAME")
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    main()
